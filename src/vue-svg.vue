fb73711e85a410fa5da2e8c2106635e8880ef938
mr-black-coffee <mr_black_coffee@126.com>
2022-08-03 15:45:17 +0800

    Add countTo to vue-svg.vue

--- src/vue-svg.vue
+++ src/vue-svg.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="vue-svg" :style="getStyle" ref="svgContainer" v-html="svgContent" v-on="innerOn"></div>
+    <div class="vue-svg" :style="getStyle" ref="svgContainer" v-on="innerOn"></div>
 </template>
 
 <script>
-import { createSvg } from 'better-svg'
+import { createSvg, SvgTextMonitor, CountTo } from 'better-svg'
 export default {
     props: {
         // svg文件地址，请传入已解析过的绝对地址，如dev或production环境中的地址
@@ -45,12 +45,50 @@ export default {
         fileLoader: {
             type: Function,
             default: async () => ''
+        },
+        // 百分比动画集合
+        percentAnimations: {
+            type: Array,
+            default: () => []
+        },
+        // 按子元素个数实现百分比动画的集合
+        percentChildren: {
+            type: Array,
+            default: () => []
+        },
+        // 是否需要监控居中文本
+        centerText:{
+            type: Boolean,
+            default: true
+        },
+        // svg中需要设置为居中的选择器
+        centerTextSelector: {
+            type: String,
+            default: '.text-m'
+        },
+        countTo: {
+            type: Array | Object,
+            default: null
+        },
+        duration: {
+            type: Number,
+            default: 3000
+        },
+        autoPlay: {
+            type: Boolean,
+            default: false
+        },
+        id: {
+            type: String,
+            default: ''
         }
     },
     data() {
         return {
             svgContent: '',
-            svgDom: null
+            svgDom: null,
+            inited: false,
+            textMonitor: null
         }
     },
     computed: {
@@ -77,44 +115,115 @@ export default {
         value: {
             deep: true,
             handler() {
-                this.updateValue()
+               this.inited && this.updateValue()
             }
-        }
+        },
+        percentAnimations: {
+            deep: true,
+            handler() {
+                this.inited && this.updatePercentAnimations()
+            }
+        },
+        percentChildren: {
+            deep: true,
+            handler() {
+                this.inited && this.updatePercentChildren()
+            }
+        },
     },
     mounted() {
         this.init()
     },
     methods: {
         async init() {
+            let _this = this
             if (!this.content && !this.src) {
                 return
             }
-            let url = this.src
-            let svgContent = this.content || await this.fileLoader(url)
+            let url = _this.src
+            let svgContent = _this.content || await loadSvg(url)
             if (svgContent) {
-                if (!this.beforeRender) {
-                    this.svgContent = svgContent.replace(/\n/g, '').replace(/^(.*)(<svg.*)/i, '$2')
-                    return
+                svgContent = svgContent.replace(/\n/g, '').replace(/^(.*)(<svg.*)/i, '$2')
+                _this.svgDom = createSvg(svgContent)
+                _this.beforeRender && _this.beforeRender(this.svgDom)
+                _this.centerText && _this.hideDom(_this.svgDom, _this.centerTextSelector)
+                
+                _this.$refs.svgContainer.appendChild(_this.svgDom)
+                let img = document.createElement('img')
+                img.src = ''
+                img.onerror = function() {
+                    _this.initTextMonitor()
+                    _this.updateValue()
+                    _this.initCountTo()
+                    this.parentNode.removeChild(this)
+                    
+                    setTimeout(() => {
+                        _this.resumeDom(_this.svgDom, _this.centerTextSelector)
+                        if (_this.autoPlay) {
+                            _this.start()
+                        }
+                        _this.inited = true
+                        _this.$emit('inited')
+                    }, 100)
                 }
-                this.svgDom = createSvg(svgContent)
+                _this.$refs.svgContainer.appendChild(img)
             }
         },
+
+        hideDom(svgDom, selector) {
+            if (svgDom && selector) {
+                let nodeList = [...svgDom.querySelectorAll(selector)]
+                nodeList.forEach(node => {
+                    let oriOpacity = node.getAttribute('opacity')
+                    if (oriOpacity) {
+                        node.setAttribute('oriOpacity', oriOpacity)
+                    }
+                    node.setAttribute('opacity', '0')
+                })
+            }
+        },
+
+        resumeDom(svgDom, selector) {
+            if (svgDom && selector) {
+                let nodeList = [...svgDom.querySelectorAll(selector)]
+                nodeList.forEach(node => {
+                    let oriOpacity = node.getAttribute('oriOpacity')
+                    if (oriOpacity) {
+                        node.setAttribute('opacity', oriOpacity)
+                        node.removeAttribute('oriOpacity')
+                        return
+                    }
+                    node.removeAttribute('opacity')
+                })
+            }
+        },
+
+        initTextMonitor() {
+            if (this.centerText) {
+                this.textMonitor = new SvgTextMonitor({
+                    container: this.svgDom,
+                    selector: this.centerTextSelector
+                })
+            }
+            this.inited = true
+        },
+
         updateValue() {
             if (this.svgDom) {
                 this.value.forEach(item => {
                     const {selector, value} = item
-                    const doms = this.svgDom.querySelecotrAll(selector)
-                    const len = doms.length
+                    const nodeList = this.svgDom.querySelectorAll(selector)
+                    const len = nodeList.length
                     if (len === 0) {
                         return
                     } else if (len === 1) {
-                        doms[0].innerHTML = Array.isArray(value)
+                        nodeList[0].innerHTML = Array.isArray(value)
                             ? value[0] ?? ''
                             : value ?? ''
                     } else {
                         // 多个值，如果value是数组则按顺序匹配，如果value不是数组则只修改第0个dom，后续dom都置空
                         for (let i = 0; i < len; i++) {
-                            doms[i].innerHTML = Array.isArray(value)
+                            nodeList[i].innerHTML = Array.isArray(value)
                                 ? value[i] ?? ''
                                 : i === 0
                                     ? value ?? ''
@@ -123,6 +232,82 @@ export default {
                     }
                 })
             }
+        },
+
+        updatePercentAnimations() {
+            if (this.svgDom) {
+                this.percentAnimations.forEach(item => {
+                    const {selector, value} = item
+                    const per = value > 1
+                        ? value / 100
+                        : value
+                    const dom = this.svgDom.querySelector(selector)
+                    if (dom) {
+                        const to = dom.getAttribute('to')
+                        const toArr = to.split(/\s+/)
+                        if (toArr && toArr.length > 1) {
+                            const val = Math.round(per * toArr[1])
+                            dom.setAttribute('to', `${val} ${toArr[1]}`)
+                        }
+                    }
+                })
+            }
+        },
+
+        updatePercentChildren() {
+            let _this = this
+            if (_this.svgDom) {
+                _this.percentChildren.forEach(async (item) => {
+                    const {selector, value, duration = _this.duration} = item
+                    const per = value > 1
+                        ? value / 100
+                        : value
+                    const dom = _this.svgDom.querySelector(selector)
+                    if (dom) {
+                        const max = dom.children.length
+                        let steps = Math.floor(max * per)
+                        if (steps === 0 && value > 0) {
+                            steps = 1
+                        }
+                        if (steps > max) {
+                            steps = max
+                        }
+                        for (let i = 0; i < steps; i++) {
+                            await updateDomOpacity(dom.children[i], duration)
+                        }
+                    }
+                })
+            }
+        },
+
+        initCountTo() {
+            if (this.countTo) {
+                const countToList = Array.isArray(this.countTo)
+                    ? this.countTo
+                    : [this.countTo]
+                this.countToList = []
+                countToList.forEach(c => {
+                    if (!c.duration) {
+                        c.duration = this.duration
+                    }
+                    this.countToList.push(new CountTo(c, this.svgDom))
+                })
+            }
+        },
+
+        startCountTo(index = -1) {
+            if (index > -1) {
+                const c = this?.countToList?.[index]
+                c && c.start()
+            } else {
+                this.countToList && this.countToList.forEach(c => c.start())
+            }
+        },
+
+        start() {
+            this.startCountTo()
+            this.updatePercentAnimations()
+            this.updatePercentChildren()
         }
     }
 }
